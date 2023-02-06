2e4eebd92613109b0561efb77dd7ad6ee3198f4f
AtharvaTathe <atharva.tathe55@gmail.com>
2023-02-06 14:40:31 +0530

    connect application with mongoDB Database

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,68 +1,59 @@
-const fs = require('fs');
+const Tour = require('./../models/tourModel');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+// );
 
-exports.checkId = (req, res, next, val) => {
-  if (req.params.id > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id',
-    });
-  }
-  next();
-};
+// exports.checkId = (req, res, next, val) => {
+//   if (req.params.id > tours.length) {
+//     return res.status(404).json({
+//       status: 'fail',
+//       message: 'Invalid Id',
+//     });
+//   }
+//   next();
+// };
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Bad request (Missing name or price)',
-    });
-  }
+// exports.checkBody = (req, res, next) => {
+//   if (!req.body.name || !req.body.price) {
+//     return res.status(400).json({
+//       status: 'fail',
+//       message: 'Bad request (Missing name or price)',
+//     });
+//   }
 
-  next();
-};
+//   next();
+// };
 
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    result: tours.length,
-    data: { tours },
+    // result: tours.length,
+    // data: { tours },
   });
 };
 
 exports.getTourById = (req, res) => {
   //   console.log(req.params);
   const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
+  // const tour = tours.find((el) => el.id === id);
 
-  res.status(200).json({
-    status: 'success',
-    data: { tours: tour },
-  });
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: { tours: tour },
+  // });
 };
 
-exports.createTour = (req, res) => {
-  //   console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
+exports.createTour = async (req, res) => {
+  const newTour = await Tour.create(req.body)
+ res.status(201).json({
         status: 'success',
         data: {
           tours: newTour,
         },
       });
-    }
-  );
 };
 
 exports.updateTour = (req, res) => {
