79b79feaab62719a75c8273ff109dfd6fc758346
AtharvaTathe <atharva.tathe55@gmail.com>
2023-02-06 14:53:00 +0530

    create Tour with DB are completed

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -47,13 +47,20 @@ exports.getTourById = (req, res) => {
 };
 
 exports.createTour = async (req, res) => {
-  const newTour = await Tour.create(req.body)
- res.status(201).json({
-        status: 'success',
-        data: {
-          tours: newTour,
-        },
-      });
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tours: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid Data sent',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
